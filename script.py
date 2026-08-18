574d4f8b96d8755b45b37e2af0096114ceb1c30d
viniciusfariac <vinicius.cerqueira@sptech.school>
2026-08-18 19:45:11 -0300

    feat: add data collection script

---
+++ script.py
@@ -0,0 +1,134 @@
+import psutil
+from getmac import get_mac_address
+import socket
+
+# MAC ADRESS
+mac = get_mac_address()
+hostname = socket.gethostname()
+
+
+# TUDO SOBRE CPU 
+
+# PORCENTAGEM CPU
+cpu_percent = psutil.cpu_percent(interval=1)
+
+# TEMPO DA CPU
+cpu_user = psutil.cpu_times().user
+cpu_system = psutil.cpu_times().system
+cpu_idle = psutil.cpu_times().idle # tempo ocioso / sem fazer nada
+cpu_nice = psutil.cpu_times().nice # tarfa de nível baixo
+cpu_iowait = psutil.cpu_times().iowait # tempo de aguardo de uma operação de entrada e saída
+cpu_irq = psutil.cpu_times().irq # interrupt quest, quando um hardware chama a cpu
+cpu_softirq = psutil.cpu_times().softirq # interrupção de software, precido com o de cima, mas nivel software
+
+
+# CONTAS DA CPU
+cpu_cores = psutil.cpu_count(logical=False)
+cpu_threads = psutil.cpu_count(logical=True)
+
+
+# MÉDIA DE NÚCLEOS POR 1 MIN 10 MIN 15 MIN
+loadavg = psutil.getloadavg()
+
+
+# TUDO SOBRE MEMÓRIA RAM
+
+# USO DA MEMÓRIA
+memory_total = psutil.virtual_memory().total
+memory_available = psutil.virtual_memory().available
+memory_used = psutil.virtual_memory().used
+memory_percent = psutil.virtual_memory().percent
+
+
+# TUDO SOBRE MEMÓRIA SWAP
+
+# MEMÓRIA SWAP EM USO
+swap_used = psutil.swap_memory().used
+swap_percent = psutil.swap_memory().percent
+
+
+# TUDO SOBRE DISCO
+
+# DESEMPENHO DO DISCO
+disk_write_time = psutil.disk_io_counters().write_time
+disk_read_time = psutil.disk_io_counters().read_time
+
+
+# TUDO SOBRE REDE
+
+# Recebimento e envio de bytes na rede
+net_bytes_recv = psutil.net_io_counters().bytes_recv
+net_bytes_sent = psutil.net_io_counters().bytes_sent
+
+# Erros de informações saindo e chegando
+net_errors_in = psutil.net_io_counters().errin
+net_errors_out = psutil.net_io_counters().errout
+
+# Informações que foram dropadas (descartadas) na rede na entrada e saída
+net_drops_in = psutil.net_io_counters().dropin
+net_drops_out = psutil.net_io_counters().dropout
+
+
+
+# BYTES PARA GB
+def bytes_para_gb(value):
+    return value / (1024 ** 3)
+
+
+# ==============================
+# RELATÓRIO
+# ==============================
+
+print(f"""
+BEM VINDO AO MELHOR MONITORAMENTO DE SISTEMA
+
+INFORMAÇÕES DO COMPUTADOR
+mac: {mac}
+hostname: {hostname}
+
+INFORMAÇÕES DA CPU
+Uso da CPU: {cpu_percent:.1f}%
+Núcleos físicos: {cpu_cores}
+Threads lógicas: {cpu_threads}
+
+Tempo de CPU:
+User: {cpu_user:.2f} segundos
+System: {cpu_system:.2f} segundos
+Idle: {cpu_idle:.2f} segundos
+Nice: {cpu_nice:.2f} segundos
+I/O Wait: {cpu_iowait:.2f} segundos
+IRQ: {cpu_irq:.2f} segundos
+SoftIRQ: {cpu_softirq:.2f} segundos
+
+Load Average:
+1 minuto: {loadavg[0]:.2f}
+5 minutos: {loadavg[1]:.2f}
+15 minutos: {loadavg[2]:.2f}
+
+
+INFORMAÇÕES DA MEMÓRIA RAM
+
+Memória total: {bytes_para_gb(memory_total):.2f} GB
+Memória disponível: {bytes_para_gb(memory_available):.2f} GB
+Memória usada: {bytes_para_gb(memory_used):.2f} GB
+Uso da memória: {memory_percent:.1f}%
+
+INFORMAÇÕES DA MEMÓRIA SWAP
+Swap usada: {bytes_para_gb(swap_used):.2f} GB
+Uso da Swap: {swap_percent:.1f}%
+
+INFORMAÇÕES DO DISCO
+Tempo de escrita: {disk_write_time} ms
+Tempo de leitura: {disk_read_time} ms
+
+INFORMAÇÕES DA REDE
+GB recebidos: {bytes_para_gb(net_bytes_recv):.2f} GB
+GB enviados: {bytes_para_gb(net_bytes_sent):.2f} GB
+
+Erros de entrada: {net_errors_in}
+Erros de saída: {net_errors_out}
+
+Pacotes descartados na entrada: {net_drops_in}
+Pacotes descartados na saída: {net_drops_out}
+
+""")
