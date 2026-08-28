7fe6f2f63c7bfa3b7840b689393c2b70599d316d
Daniel Costa <daniel.csilva@sptech.school>
2026-08-28 00:01:00 -0300

    fix: update script.py

--- script.py
+++ script.py
@@ -5,87 +5,138 @@ import csv
 from datetime import datetime
 
 # MAC ADRESS
-mac = get_mac_address()
-hostname = socket.gethostname()
+def info_machine():
+    return{
+    "mac": get_mac_address(),
+    "hostname": socket.gethostname()
+}
 
 
 # TUDO SOBRE CPU 
+def cpu_functions():
+    # PORCENTAGEM CPU
 
-# PORCENTAGEM CPU
-cpu_percent = psutil.cpu_percent(interval=1)
+    # TEMPO DA CPU
+    cpu_percent = psutil.cpu_percent(interval=1)
+    cpu_time = psutil.cpu_times()
+    
+    return { 
+    "cpu_percent": cpu_percent,
+    "cpu_user": cpu_time.user,
+    "cpu_system": cpu_time.system,
+    "cpu_idle": cpu_time.idle, # tempo ocioso / sem fazer nada
 
-# TEMPO DA CPU
-cpu_user = psutil.cpu_times().user
-cpu_system = psutil.cpu_times().system
-cpu_idle = psutil.cpu_times().idle # tempo ocioso / sem fazer nada
-cpu_nice = psutil.cpu_times().nice # tarfa de nível baixo
-cpu_iowait = psutil.cpu_times().iowait # tempo de aguardo de uma operação de entrada e saída
-cpu_irq = psutil.cpu_times().irq # interrupt quest, quando um hardware chama a cpu
-cpu_softirq = psutil.cpu_times().softirq # interrupção de software, precido com o de cima, mas nivel software
+    # funções linux
+    #"cpu_nice": cpu_time.nice, # tarfa de nível baixo
+    #"cpu_iowait": cpu_time.iowait, # tempo de aguardo de uma operação de entrada e saída
+    #"cpu_irq": cpu_time.irq, # interrupt quest, quando um hardware chama a cpu
+    #"cpu_softirq": cpu_time.softirq, # interrupção de software, precido com o de cima, mas nivel software
 
 
-# CONTAS DA CPU
-cpu_cores = psutil.cpu_count(logical=False)
-cpu_threads = psutil.cpu_count(logical=True)
-
+    # CONTAS DA CPU
+    "cpu_cores" : psutil.cpu_count(logical=False),
+    "cpu_threads": psutil.cpu_count(logical=True)
+    }
 
 # MÉDIA DE NÚCLEOS POR 1 MIN 10 MIN 15 MIN
-loadavg = psutil.getloadavg()
-
-
-# TUDO SOBRE MEMÓRIA RAM
-
-# USO DA MEMÓRIA
-memory_total = psutil.virtual_memory().total
-memory_available = psutil.virtual_memory().available
-memory_used = psutil.virtual_memory().used
-memory_percent = psutil.virtual_memory().percent
-
-
-# TUDO SOBRE MEMÓRIA SWAP
-
-# MEMÓRIA SWAP EM USO
-swap_used = psutil.swap_memory().used
-swap_percent = psutil.swap_memory().percent
-
-
-# TUDO SOBRE DISCO
-
-# DESEMPENHO DO DISCO
-disk_write_time = psutil.disk_io_counters().write_time
-disk_read_time = psutil.disk_io_counters().read_time
-
-
-# TUDO SOBRE REDE
-
-# Recebimento e envio de bytes na rede
-net_bytes_recv = psutil.net_io_counters().bytes_recv
-net_bytes_sent = psutil.net_io_counters().bytes_sent
-
-# Erros de informações saindo e chegando
-net_errors_in = psutil.net_io_counters().errin
-net_errors_out = psutil.net_io_counters().errout
-
-# Informações que foram dropadas (descartadas) na rede na entrada e saída
-net_drops_in = psutil.net_io_counters().dropin
-net_drops_out = psutil.net_io_counters().dropout
-
-
+# função linux para indicar a quantidade média de trabalho/processos aguardando ou utilizando CPU
+#def loadavg_function():
+#    loadavg = psutil.getloadavg()
+ #   return{
+  #     "loadavg_1min": loadavg[0],
+   #     "loadavg_5min": loadavg[1],
+    #    "loadavg_15min": loadavg[2]
+    #}
 
 # BYTES PARA GB
 def bytes_para_gb(value):
     return value / (1024 ** 3)
 
+# TUDO SOBRE MEMÓRIA RAM
+def memory_functions():
+    # USO DA MEMÓRIA
+    memory = psutil.virtual_memory()
+    swap = psutil.swap_memory()
 
+    return {
+    "memory_total_gb": bytes_para_gb(memory.total),
+    "memory_available": bytes_para_gb(memory.available),
+    "memory_used": bytes_para_gb(memory.used),
+    "memory_percent": memory.percent,
+    # TUDO SOBRE MEMÓRIA SWAP
+    # MEMÓRIA SWAP EM USO
+    "swap_used": bytes_para_gb(swap.used),
+    "swap_percent": swap.percent
+    }
+
+
+
+# TUDO SOBRE DISCO
+def disk_functions():
+    # DESEMPENHO DO DISCO
+    disk = psutil.disk_io_counters()
+
+    return{
+    "disk_write_time": disk.write_time,
+    "disk_read_time": disk.read_time,
+    "disk_read_bytes": bytes_para_gb(disk.read_bytes),
+    "disk_write_bytes": bytes_para_gb(disk.write_bytes),
+    "disk_read_count": disk.read_count,
+    "disk_write_count": disk.write_count
+    }
+    
+
+# TUDO SOBRE REDE
+def network_functions():
+    # Recebimento e envio de bytes na rede
+    network = psutil.net_io_counters()
+
+    return{
+    "net_bytes_recv": bytes_para_gb(network.bytes_recv),
+    "net_bytes_sent": bytes_para_gb(network.bytes_sent),
+    # Erros de informações saindo e chegando
+    "net_errors_in": network.errin,
+    "net_errors_out": network.errout,
+     # Informações que foram dropadas (descartadas) na rede na entrada e saída
+    "net_drops_in": network.dropin,
+    "net_drops_out": network.dropout
+    }
+    
+    
+
+# temperatura, apenas no linux
+#def temperature_measure():
+ #   return psutil.sensors_temperatures()
+
+
+
+#loadavg = loadavg_function()
+cpu = cpu_functions()
+memory = memory_functions()
+disk = disk_functions()
+network = network_functions()
+#temperature = temperature_measure()
+machine = info_machine()
 date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-dados = [
-    date_time, mac, hostname, cpu_percent, cpu_cores, cpu_threads, cpu_user, cpu_system, cpu_idle, cpu_nice, cpu_iowait, cpu_irq, cpu_softirq, loadavg[0], loadavg[1], loadavg[2], bytes_para_gb(memory_total), bytes_para_gb(memory_available), bytes_para_gb(memory_used), memory_percent, bytes_para_gb(swap_used), swap_percent, disk_write_time, disk_read_time, bytes_para_gb(net_bytes_recv), bytes_para_gb(net_bytes_sent), net_errors_in, net_errors_out, net_drops_in, net_drops_out
-]
+# ** significa dicionary unpacking, para juntar os dicionários
+dados = {
+    "timestamp": date_time,
+    **machine,
+    **cpu,
+    **memory,
+    **disk,
+    **network
+}
 
 with open ("./relatorio_monitoramento.csv", "a") as csv_file:
-    csv.writer(csv_file, delimiter=";").writerow(dados)
+    writer = csv.DictWriter(
+        csv_file,
+        fieldnames=dados.keys(),
+        delimiter=";"
+    )
 
+    writer.writerow(dados)
 
 # ==============================
 # RELATÓRIO
@@ -97,52 +148,51 @@ BEM VINDO AO SENTRY, SEU MELHOR MONITORAMENTO DO SISTEMA SCADA
 Horário do monitoramento: {date_time}
 
 INFORMAÇÕES DO COMPUTADOR
-mac: {mac}
-hostname: {hostname}
+mac: {machine["mac"]}
+hostname: {machine["hostname"]}
 
 INFORMAÇÕES DA CPU
-Uso da CPU: {cpu_percent:.1f}%
-Núcleos físicos: {cpu_cores}
-Threads lógicas: {cpu_threads}
+Uso da CPU: {cpu["cpu_percent"]:.1f}%
+Núcleos físicos: {cpu["cpu_cores"]}
+Threads lógicas: {cpu["cpu_threads"]}
 
 Tempo de CPU:
-User: {cpu_user:.2f} segundos
-System: {cpu_system:.2f} segundos
-Idle: {cpu_idle:.2f} segundos
-Nice: {cpu_nice:.2f} segundos
-I/O Wait: {cpu_iowait:.2f} segundos
-IRQ: {cpu_irq:.2f} segundos
-SoftIRQ: {cpu_softirq:.2f} segundos
-
-Load Average:
-1 minuto: {loadavg[0]:.2f}
-5 minutos: {loadavg[1]:.2f}
-15 minutos: {loadavg[2]:.2f}
+User: {cpu["cpu_user"]:.2f} segundos
+System: {cpu["cpu_system"]:.2f} segundos
+Idle: {cpu["cpu_idle"]:.2f} segundos
 
 
 INFORMAÇÕES DA MEMÓRIA RAM
 
-Memória total: {bytes_para_gb(memory_total):.2f} GB
-Memória disponível: {bytes_para_gb(memory_available):.2f} GB
-Memória usada: {bytes_para_gb(memory_used):.2f} GB
-Uso da memória: {memory_percent:.1f}%
+Memória total: {memory["memory_total_gb"]:.2f} GB
+Memória disponível: {memory["memory_available"]:.2f} GB
+Memória usada: {memory["memory_used"]:.2f} GB
+Uso da memória: {memory["memory_percent"]:.1f}%
 
 INFORMAÇÕES DA MEMÓRIA SWAP
-Swap usada: {bytes_para_gb(swap_used):.2f} GB
-Uso da Swap: {swap_percent:.1f}%
+Swap usada: {memory["swap_used"]:.2f} GB
+Uso da Swap: {memory["swap_percent"]:.1f}%
 
 INFORMAÇÕES DO DISCO
-Tempo de escrita: {disk_write_time} ms
-Tempo de leitura: {disk_read_time} ms
+Tempo de escrita: {disk["disk_write_time"]} ms
+Tempo de leitura: {disk["disk_read_time"]} ms
+
+Dados lidos: {disk["disk_read_bytes"]:.2f} GB
+Dados escritos: {disk["disk_write_bytes"]:.2f} GB
+
+Operações de leitura:  {disk["disk_read_count"]}
+Operações de escrita:  {disk["disk_write_count"]}
 
 INFORMAÇÕES DA REDE
-GB recebidos: {bytes_para_gb(net_bytes_recv):.2f} GB
-GB enviados: {bytes_para_gb(net_bytes_sent):.2f} GB
+GB recebidos: {network["net_bytes_recv"]:.2f} GB
+GB enviados: {network["net_bytes_sent"]:.2f} GB
 
-Erros de entrada: {net_errors_in}
-Erros de saída: {net_errors_out}
+Erros de entrada: {network["net_errors_in"]}
+Erros de saída: {network["net_errors_out"]}
 
-Pacotes descartados na entrada: {net_drops_in}
-Pacotes descartados na saída: {net_drops_out}
+Pacotes descartados na entrada: {network["net_drops_in"]}
+Pacotes descartados na saída: {network["net_drops_out"]}
 
 """)
+
+print(dados)
